3b74bde8873003d5740674f07c073d7bac2d778e
Lugi <47616503+ainonim@users.noreply.github.com>
2019-04-28 19:30:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+
+
+
+<?php
+function bacaHTML($url){
+         // inisialisasi CURL
+         $data = curl_init();
+         // setting CURL
+         curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+         curl_setopt($data, CURLOPT_URL, $url);
+         // menjalankan CURL untuk membaca isi file
+         $hasil = curl_exec($data);
+         curl_close($data);
+         return $hasil;
+    }
+
+    // Grabbing from Mediafire.com
+    $mediafire_HTML =  bacaHTML('https://www.mediafire.com/file/qux7rrh2p36yjez/Astagfirullah.mp4'); // url content
+    
+    // Mengambil direct link download
+    $mediafire_geturl = explode('                        href="', $mediafire_HTML); // tag awal
+    $mediafire_getlink = explode('">', $mediafire_geturl[1]); // akhir tag 
+   
+    // Mengambil judul file 
+    $mediafire_judul = explode('                            <div class="filename">' , $mediafire_HTML); // tag awal
+    $mediafire_getjudul = explode('</div>' , $mediafire_judul[1]); //akhir tag   
+
+    // Menampilkan hasil
+    echo'Judul : '.$mediafire_getjudul[0].' </br>';
+    echo'Link : '.$mediafire_getlink[0].' </br>';
+?>
